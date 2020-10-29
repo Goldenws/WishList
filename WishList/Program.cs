ec932efbbf7960c017b444547062aa02c5bb1a6e
RolandStrod <KhaleAldeAsad@gmail.com>
2020-10-29 11:34:48 +0200

    WishList

---
+++ WishList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WishList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Roland Strod\Data\WishList.txt";
+            List<string> dataFromFile = File.ReadAllLines(filePath).ToList();
+
+            foreach(string line in dataFromFile)
+            {
+                Console.WriteLine($"Data from file: {line}");
+            }
+
+            dataFromFile.Add("Minecraft");
+            File.WriteAllLines(filePath, dataFromFile);
+
+        }
+    }
+}
---
+++ WorkingWithFiles/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace WorkingWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Roland Strod\Data\test.txt";
+            List<string> dataFromFile = File.ReadAllLines(filePath).ToList();
+
+            foreach(string line in dataFromFile)
+            {
+                Console.WriteLine($"Data from file: {line}");
+            }
+
+            dataFromFile.Add("grapes");
+            File.WriteAllLines(filePath, dataFromFile);
+        }
+    }
+}
